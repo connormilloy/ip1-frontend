f4f60ef6d66ce052345dcd0f75f2e4315bcfcb62
Connor <connorking.milloy@gmail.com>
2022-08-15 15:21:48 +0100

    date formatting

--- src/Components/Dashboard/Appointments/ViewAllAppointments/AppointmentsTable/Appointment/Appointment.js
+++ src/Components/Dashboard/Appointments/ViewAllAppointments/AppointmentsTable/Appointment/Appointment.js
@@ -1,12 +1,17 @@
 import styles from './Appointment.module.scss';
 import StatusLozenge from './StatusLozenge/StatusLozenge';
+import moment from 'moment';
 
 const Appointment = ({ apt, level, onClick }) => {
+    const formatAppointment = dt => {
+        return moment(dt).format("DD/MM/YYYY HH:mm:ss");
+    }
+    
     return(
         <tr className={styles.appointment} onClick={() => onClick(apt)}>
             <td>{level == 1 ? apt.Salesperson : apt.User}</td>
             {level == 2 && <td>{apt.Company}</td>}
-            <td>{apt.Appointment}</td>
+            <td>{formatAppointment(apt.Appointment)}</td>
             <td className={styles.lozenge}>{<StatusLozenge status={apt.hasPassed}/>}</td>
         </tr>
     )
